f79b885e7025aeaa35135a8e1daf8599c09b08f2
Ian Page Hands <iphands@gmail.com>
2010-01-17 13:30:19 -0500

    largetext.user.js: added quick buttons

--- progs/greasemonkey/largetext.user.js
+++ progs/greasemonkey/largetext.user.js
@@ -24,17 +24,23 @@
 */
 
 
-unsafeWindow.iphands_toggle_size = function(type, state)
+unsafeWindow.iphands_toggle_size = function(type)
 {
-    if (state)
+    
+    var state_elem = document.getElementById(type + "_state");
+    var state_button = document.getElementById(type + "_button");
+    
+    if (state_elem.innerHTML == 1)
 	{
 	    myFontSize = '';
-	    state = 0;
+	    state_elem.innerHTML = 0;
+	    state_button.style.color = "";
 	}
     else
 	{
-	    myFontSize = 'xx-large';	    
-	    state = 1;
+	    myFontSize = 'xx-large';
+	    state_elem.innerHTML = 1;
+	    state_button.style.color = "red";
 	}
 
     var elements = document.getElementsByTagName(type);
@@ -54,17 +60,22 @@ function top_bar()
     menuTable.id = 'iphands_menuTable';
     menuTable.innerHTML = '<table>' +
 	'<tr>' +
-	'<td><input type="button" value="div" onclick="iphands_toggle_size(\'div\', divState)"></input></td>' +
-	'<td><input type="button" value="p" onclick="iphands_toggle_size(\'p\', 0)"></input></td>' +
-	'<td><input type="button" value="a" onclick="iphands_toggle_size(\'a\', 0)"></input></td>' +
+	'<td><input id="div_button" type="button" value="div" onclick="iphands_toggle_size(\'div\')"></input></td>' +
+	'<td><input id="p_button" type="button" value="p" onclick="iphands_toggle_size(\'p\')"></input></td>' +
+	'<td><input id="a_button" type="button" value="a" onclick="iphands_toggle_size(\'a\')"></input></td>' +
+	'<td><input id="td_button" type="button" value="td" onclick="iphands_toggle_size(\'td\')"></input></td>' +
 	'</tr>' +
 	'</table>' +
+	'<a id="div_state" style="visibility: hidden">0</a>'+
+	'<a id="p_state" style="visibility: hidden">0</a>'+
+	'<a id="a_state" style="visibility: hidden">0</a>'+
+	'<a id="td_state" style="visibility: hidden">0</a>'+
 	'\n';
     
     var firstBodyElem = document.getElementsByTagName('body')[0].childNodes[0];
     firstBodyElem.parentNode.insertBefore(menuTable, firstBodyElem);
 }
 
-//top_bar();
-unsafeWindow.iphands_toggle_size('div', 0);
-unsafeWindow.iphands_toggle_size('td', 0);
+top_bar();
+unsafeWindow.iphands_toggle_size('div');
+unsafeWindow.iphands_toggle_size('td');
